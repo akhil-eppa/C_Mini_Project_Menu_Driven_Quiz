3f3ba97bebaea6893bbdc3faae5f549d72b6a505
akhil-eppa <42675875+akhil-eppa@users.noreply.github.com>
2019-03-24 17:00:49 +0530

    Update client.c

--- client.c
+++ client.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-    printf("Welcome to Menu Driven Quiz\nRULES:\n->Each correct answer fetches 4 points\n->No negative marking\nHit Enter to Begin!\nPress q followed by Enter to exit\n");
+    printf("Welcome to Menu Driven Quiz\nRULES:\n->Each correct answer fetches 4 points\n->An incorrect answer gives (-2)\n->You can make a choice from 4 categories\n\n||Hit Enter to Begin!||\n(Press [q followed by enter] to exit)\n");
     char x;int ch=1;
     while((x=getchar())!='\n')
     {
@@ -16,6 +16,12 @@ int main()
 	printf("Do you want to take the quiz again?(1 for yes and 0 for no) :");
 	
 	scanf("%d",&ch);
+	if (ch==1)
+		system("clear");
+	else if (ch==0)
+	{
+		printf("\n\nThanks for taking the quiz! See you soon.....\n\n");
+	}
 	}
     return 0;
 }
